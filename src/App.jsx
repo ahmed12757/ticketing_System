8a9f4a56b7f87f95ed48f975cc06f57d3bd6872b
ahmed12757 <ah8962083@gmail.com>
2025-02-10 23:42:58 +0200

    make login main page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import { Toaster } from "react-hot-toast";
 import Home from "./page/Home/Home";
 import ForgetPassword from "./page/Forget Password/ForgetPassword";
 import ResetPassword from "./page/reset password/ResetPassword";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "signup", element: <Signup /> },
-    { path: "login", element: <Login /> },
-    { path: "home", element: <Home /> },
-    { path: "forgetPassword", element: <ForgetPassword /> },
-    { path: "resetPassword", element: <ResetPassword /> },
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "signup", element: <Signup /> },
+        { index: true, element: <Login /> },
+        { path: "login", element: <Login /> },
+        { path: "home", element: <Home /> },
+        { path: "forgetPassword", element: <ForgetPassword /> },
+        { path: "resetPassword", element: <ResetPassword /> },
+      ],
+    },
   ]);
 
   return (
